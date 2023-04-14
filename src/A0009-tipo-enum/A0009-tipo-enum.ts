5f2605c3c56d988ceb2cc5d556536168436a374f
RaphaelGMoreira <raphaelgimenesmoreira@gmail.com>
2023-04-14 09:13:22 -0300

    tipo-enum

---
+++ src/A0009-tipo-enum/A0009-tipo-enum.ts
@@ -0,0 +1,21 @@
+enum Cores {
+  VERMELHO, // 0
+  AZUL, // 1
+  AMARELO, // 2
+}
+
+enum Cores {
+  ROXO = 'ROXO',
+  VERDE = 201,
+  ROSA,
+}
+
+// console.log(Cores);
+// console.log(Cores.VERMELHO);
+// console.log(Cores[0]);
+
+function escolhaACor(cor: Cores): void {
+  console.log(Cores[cor]);
+}
+
+escolhaACor(Cores.ROXO);
