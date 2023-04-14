4021dad597935f53e347c818d4a48053d9362e98
RaphaelGMoreira <raphaelgimenesmoreira@gmail.com>
2023-04-14 09:22:55 -0300

    tipo-unknown

---
+++ src/A0010-tipo-unknown/A0010-tipo-unknown.ts
@@ -0,0 +1,13 @@
+// unknown é um any, só que mais seguro
+let x: unknown;
+
+x = 100;
+x = 'Luiz';
+x = 900;
+x = '10';
+const y = 800;
+
+// console.log(x + y);
+
+if (typeof x === 'number') console.log(x + y);
+console.log('Precisa ser um numero');
