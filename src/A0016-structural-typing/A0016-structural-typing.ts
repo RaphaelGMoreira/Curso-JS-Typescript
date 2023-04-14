f5ea5a30c22b5215f606289d2eaccfc4668ed229
RaphaelGMoreira <raphaelgimenesmoreira@gmail.com>
2023-04-14 10:56:16 -0300

    structural-typing

---
+++ src/A0016-structural-typing/A0016-structural-typing.ts
@@ -0,0 +1,13 @@
+type VerifyUserFn = (user: User, sentValue: User) => boolean;
+type User = { username: string; password: string };
+
+const verifyUser: VerifyUserFn = (user, setValue) => {
+  return (
+    user.username === setValue.username && user.password === setValue.password
+  );
+};
+
+const bdUser = { username: 'joao', password: '123456' };
+const sentUser = { username: 'joao', password: '123456' };
+const loggedIn = verifyUser(bdUser, sentUser);
+console.log(loggedIn);
