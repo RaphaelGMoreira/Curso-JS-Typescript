f9e367b12862e0a35dcbe3ecf9ca6b2affee9656
RaphaelGMoreira <raphaelgimenesmoreira@gmail.com>
2023-05-17 12:42:25 -0300

    A0052-decorator-class-2

---
+++ src/A0052-decorator-class-2/A0052-decorator-class-2.ts
@@ -0,0 +1,38 @@
+@inverteNomeECor
+export class Animal {
+  constructor(public nome: string, public cor: string) {
+    console.log('Sou a classe');
+  }
+}
+
+// function decorator(target: any): any {
+//   console.log('Decorator');
+//   return target;
+// }
+
+function inverteNomeECor<T extends new (...args: any[]) => any>(target: T): T {
+  console.log('Decorator e recebi ', target);
+  return class extends target {
+    cor: string;
+    nome: string;
+
+    // constructor(...args: any[]) {
+    //   super(...args);
+    //   // this.cor = 'qualquer coisa';
+    //   this.cor = args[0].split('').reverse().join('');
+    // }
+
+    constructor(...args: any[]) {
+      super(...args);
+      this.nome = this.inverte(args[0]);
+      this.cor = this.inverte(args[1]);
+    }
+
+    inverte(valor: string): string {
+      return valor.split('').reverse().join('');
+    }
+  };
+}
+
+// const animal = new Animal('Luiz', 'Roxo');
+// console.log(animal);
